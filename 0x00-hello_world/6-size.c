b72fdaeefb247685c2cf653301e712e78ef0462c
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-07-14 05:55:18 -0700

    Size in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	printf("Size of a char: \n%c byte\(s\)", sizeof(char));
-	printf("Size of an int: \n%i byte\(s\)", sizeof(int));
-	printf("Size of a long int: \n%li byte\(s\)", sizeof(long int));
-	printf("Size of a long long int: \n%lli byte\(s\)", sizeof(long long int));
-	printf("Size of a float: \n%f byte\(s\)", sizeof(float));
+	printf("Size of a char: \n%ld byte(s)", sizeof(char));
+	printf("Size of an int: \n%li byte(s)", sizeof(int));
+	printf("Size of a long int: \n%li byte(s)", sizeof(long int));
+	printf("Size of a long long int: \n%ld byte(s)", sizeof(long int));
+	printf("Size of a float: \n%ld byte(s)", sizeof(float));
 
 	return (0);
 }
