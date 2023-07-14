ca659d16599cac108e029510d165c95b71ffd18a
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-07-14 04:00:04 -0700

    Use puts to print string in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,9 @@
 #include <stdio.h>
 int main(void)
 {
-	char myString[] = "\"Programming is like building a multilingual puzzle";
+	char myString = "\"Programming is like building a multilingual puzzle";
 
 	puts(myString);
+
+	return (0);
 }
