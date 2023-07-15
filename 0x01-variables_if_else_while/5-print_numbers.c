6d2e120605e5a977ee6c393c42b0611151593487
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-07-15 16:41:35 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Function to print 10 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
