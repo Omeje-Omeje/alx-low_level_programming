c98e58975f3f8b319b7b5e7d9953fa32de2ebac7
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-07-15 09:17:19 -0700

    Print alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Function to print alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	return (0);
+}
