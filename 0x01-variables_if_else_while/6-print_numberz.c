5fec46b1ca161d7b25193fe48a8144045c5f7a4e
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-07-15 17:05:51 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Function to print numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar('0' + n);
+	}
+	putchar('\n');
+
+	return (0);
+}
