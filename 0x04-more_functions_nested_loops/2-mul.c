68ce26a97c45fa0a1291c4473a92129e4bc03c07
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-07-21 14:12:19 -0700

    Function that multipy two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Function that multiply two numbers
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	int mul =  (a * b);
+	return ( mul);
+	return (0);
+}
