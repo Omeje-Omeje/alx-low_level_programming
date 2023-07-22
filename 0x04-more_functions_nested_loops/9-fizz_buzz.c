b84836b0f840e25c5302d18e40e0b2c063ee9495
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-07-21 18:30:49 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - Function to print Fizz Buzz
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, n = 0;
+
+	for (i = 1; i <= 100; i++, n++)
+	{
+		if (n > 0)
+		{
+			putchar(' ');
+		}
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
