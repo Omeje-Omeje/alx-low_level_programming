a3fe42b337a07903815743c4900554bdedda92f7
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-07-27 08:28:55 -0700

    Concatenate string function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - Funtion to concatenete string
+ * @dest: First operand
+ * @src: Second operand
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *begin = dest;
+
+	while (*dest != '\0')
+	{
+		*dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (begin);
+}
