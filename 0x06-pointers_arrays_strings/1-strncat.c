87e1366b6b6bacd31b1dd36290ad91bbcd0c0a94
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-07-28 08:19:02 -0700

    Concatenate string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - Function to concatenate certain number of string
+ * @dest: Param one
+ * @src: Param two
+ * @n: Param three
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
