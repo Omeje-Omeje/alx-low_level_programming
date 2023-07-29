8a52ebcde727e292ca6cdeec020d95b5b3f69acc
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-07-29 09:24:22 -0700

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,14 @@
 
 int print_last_digit(int n)
 {
-	int lastDigit = n % 10;
+	int lastDigit;
 
-	return lastDigit;
+	lastDigit = n % 10;
 
+	if (n < 0)
+	{
+		lastDigit = (-1) * lastDigit;
+	}
+	_putchar('0' + lastDigit);
+	return (lastDigit);
 }
