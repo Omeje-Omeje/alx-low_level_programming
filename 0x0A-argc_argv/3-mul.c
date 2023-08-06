68c121c851abb00b488fc097f6ba023bcc22fd65
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-08-06 15:48:03 -0700

    Multiply two integers and print the product

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Function that multiply two numbers
+ * @argc: First operand
+ * @argv: Second operand
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int product;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		product = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", product);
+	}
+	return (0);
+}
