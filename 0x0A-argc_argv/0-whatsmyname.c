e5f82f5e2199a6a298c4a0227792a244eed93d25
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-08-06 15:17:36 -0700

    Print the name of the executable file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Function to print the first argument
+ * @argc: First operand
+ * @argv: Second operand
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
