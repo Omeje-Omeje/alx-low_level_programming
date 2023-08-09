20e084a8f5ad208ca2627f3ed31ac36cba6d4e9a
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-08-09 07:58:28 -0700

    Concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,13 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	char *ptr;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	i = j = 0;
 	while (s1[i] != '\0')
