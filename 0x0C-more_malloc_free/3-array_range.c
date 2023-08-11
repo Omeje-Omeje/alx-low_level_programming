34246f959b3b190eed51e8d32ac0bd9db3eb239c
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-08-10 17:54:16 -0700

    Print array of integers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * array_range - Function that creates an array of integers
+ * @min: First operand
+ * @max: Second operand
+ *
+ * Return: Always 0 (Success)
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+	int *ptr;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(int) * (max - min) + 1);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = min; i <= max; i++)
+	{
+		ptr[i] = min++;
+	}
+	return (ptr);
+}
