eb4192176869e3bf1a22f630f45a81c1af7e4ebb
Omeje-Omeje <vitus.omeje2015@yahoo.com>
2023-08-11 10:18:17 -0700

    Add numbers function

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,8 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 /**
- * main - Print sum of positive numbers
+ * num_check - Function to check for numbers
+ * @s: First operand
+ *
+ * Return: Always 0 (Success)
+ */
+
+int num_check(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (i < strlen(s))
+	{
+		if (!isdigit(s[i]))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - Function to print sum of numbers
  * @argc: First operand
  * @argv: Second operand
  *
@@ -14,17 +39,21 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc < 1)
+	i = 1;
+
+	while (i < argc)
 	{
-		printf("0\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		if (num_check(argv[i]))
 		{
 			sum += atoi(argv[i]);
 		}
-		printf("%d\n", sum);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
 	}
+	printf("%d\n", sum);
 	return (0);
 }
